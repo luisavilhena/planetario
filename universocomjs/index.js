4c292bcd5fc17f1b643c8c44c42d25dacfabcc29
Habemus <hello@habem.us>
2018-10-03 19:01:20 -0300

    scroll

--- universocomjs/index.js
+++ universocomjs/index.js
@@ -1,10 +1,11 @@
 function setUp(){
-  var planetaElement = Array.from(document.querySelectorAll('#planeta'))
-    console.log(planetaElement)
+  // var planetaElement = Array.from(document.querySelectorAll('#planeta'))
+  var planetaElement2 = document.getElementById("planeta")
+    console.log(planetaElement2)
 
   window.addEventListener('scroll', function(){
-    console.log(scrollY, planetaElement)
-    planetaElement.style.transform = "rotate(7deg)";
+    console.log(scrollY, planetaElement2)
+    planetaElement2.style.transform = "rotate(" + scrollY + "deg)" 
   })
 
 }
